376dc4f8e2d9085d128539b6befa06c8b42e8802
otherchris <chriscaragianis@gmail.com>
2022-05-20 22:16:38 -0400

    step 1

--- src/Survivor.hs
+++ src/Survivor.hs
@@ -3,8 +3,18 @@ module Survivor where
 data Survivor = Survivor
   { name :: String,
     wounds :: Int,
-    actionsRemaining :: Int
+    actionsRemaining :: Int,
+    alive :: Bool
   }
+  deriving (Eq, Show)
+
+defaultSurvivor = Survivor {name = "default", wounds = 0, actionsRemaining = 3, alive = True}
 
 newSurvivor :: String -> Survivor
-newSurvivor name = Survivor {name = name, wounds = 0, actionsRemaining = 3}
+newSurvivor name = defaultSurvivor {name = name}
+
+wound :: Survivor -> Survivor
+wound s
+  | wounds s == 0 = s {wounds = 1}
+  | wounds s == 1 = s {wounds = 2, alive = False}
+  | otherwise = s
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,21 +1,29 @@
 import Survivor
 import Test.Hspec (describe, hspec, it, shouldBe)
+import Test.Hspec.QuickCheck
 
 main :: IO ()
 main = hspec $ do
-  -- Step 1: Survivors
-  --
-  -- At this point the differences between the functional and OO
-  -- approaches is kind of superficial. `newSurvivor` almost looks
-  -- like a constructor of a Survivor class. In the functional
-  -- version, Survivor is a _type_ instead of a class.
-
   describe "newSurvivor" $ do
     it "applies the given name" $ do
-      name (newSurvivor "Chad") `shouldBe` "Chad"
+      (name . newSurvivor) "Chad" `shouldBe` "Chad"
 
-    it "new zombie has no wounds" $ do
-      wounds (newSurvivor "Chad") `shouldBe` 0
+    it "new survivor has no wounds" $ do
+      (wounds . newSurvivor) "Chad" `shouldBe` 0
 
-    it "new zombie has 3 remaining actions" $ do
-      actionsRemaining (newSurvivor "Chad") `shouldBe` 3
+    it "new survivor has 3 remaining actions" $ do
+      (actionsRemaining . newSurvivor) "Chad" `shouldBe` 3
+
+  describe "wound" $ do
+    it "increases the wounds of a Survivor by one" $ do
+      (wounds . wound . newSurvivor) "Chad" `shouldBe` 1
+
+    it "does not increase wounds past 2" $ do
+      let s = newSurvivor "Chad"
+          s' = s {wounds = 2}
+      wound s' `shouldBe` s'
+
+    it "causes death if wounds is to be 2" $ do
+      let s = newSurvivor "Chad"
+          s' = s {wounds = 1}
+      (alive . wound) s' `shouldBe` False
